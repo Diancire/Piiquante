86cfa7ac3711ae7599e4fad3e30761031210c818
Dianciré Diallo <diaancir@gmail.com>
2022-10-06 17:49:38 +0100

    Construction du parcours utilisateur signup + login

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
 
 const app = express();
-
-const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://Diancire:d97RQih6F4p8QIXA@cluster0.1cdoryv.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -13,29 +14,12 @@ mongoose.connect('mongodb+srv://Diancire:d97RQih6F4p8QIXA@cluster0.1cdoryv.mongo
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
